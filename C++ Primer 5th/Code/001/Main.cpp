e149cd5abe6529a76e0845b8ff39c428ca000f6e
billionQY <billionQY@gmail.com>
2021-01-08 16:23:40 +0800

    Create Main.cpp

---
+++ C++ Primer 5th/Code/001/Main.cpp
@@ -0,0 +1,14 @@
+// Main.cpp
+// Created by xiaojl Jan/08/2021
+// 主函数
+
+#include <iostream>
+#include "Sales_item.h"
+
+int main()
+{
+    Sales_item book;
+    std::cin >> book; // 0-201-70353-x 4 24.99
+    std::cout << book << std::endl;
+    return 0;
+}
